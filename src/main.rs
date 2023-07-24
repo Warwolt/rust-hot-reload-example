70c488fcb2da1fccbf8d87b44b222356df10c8bc
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-07-24 15:39:22 +0200

    Add logging to main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use simple_logger::SimpleLogger;
+
 #[hot_lib_reloader::hot_module(dylib = "app")]
 mod hot_app {
     pub use app::State;
     hot_functions_from_file!("app/src/lib.rs");
 }
 
+fn init_logging() {
+    SimpleLogger::new()
+        .with_module_level("hot_lib_reloader", log::LevelFilter::Info)
+        .init()
+        .unwrap();
+}
+
 fn main() {
+    init_logging();
+    log::info!("Program start");
+
     let mut state = hot_app::State::new();
     loop {
         hot_app::update(&mut state);
